37cb000046190ec87850a6b6f55c695e207153e3
HSH96\clxkd <clxkdi123@gmail.com>
2024-08-09 15:06:09 +0900

    구조 설계

---
+++ src/test/java/com/psproject/PsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
