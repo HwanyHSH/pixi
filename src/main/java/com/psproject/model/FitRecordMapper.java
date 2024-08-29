aff2256e38f65b2b74badcbb9d6016b6a14fb9dd
김진우 <ftvkal98@gmail.com>
2024-08-29 16:23:47 +0900

    운동 종류 선택 페이지 하는중

--- src/main/java/com/psproject/model/FitRecordMapper.java
+++ src/main/java/com/psproject/model/FitRecordMapper.java
@@ -2,6 +2,11 @@ package com.psproject.model;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface FitRecordMapper {
+
+    List<FitRecordDTO> fitrecord();
+
 }
